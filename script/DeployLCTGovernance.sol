1cd63785ccfdacfe203a126f19d0a70f693c9324
Dmitry Sorokin <soradimichi@gmail.com>
2025-02-09 19:50:02 +0600

    something added

--- script/DeployLCTGovernance.sol
+++ script/DeployLCTGovernance.sol
@@ -12,26 +12,27 @@ contract DeployLCTGovernance is Script {
 
         LCToken lcToken = new LCToken(50_000 ether);
         LCTGovernance governance = new LCTGovernance(lcToken, 1 ether);
-        address user = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
 
-        lcToken.transfer(user, 300 ether);
-
-        uint256 currentDay = block.timestamp / 1 days;
-        governance.createProposal(
-            "Increase Rewards",
-            "Proposal to increase reward distribution by 10%",
-            currentDay + 7 days
-        );
-        governance.createProposal(
-            "Reduce Fees",
-            "Proposal to reduce transaction fees",
-            currentDay + 10 days
-        );
-        governance.createProposal(
-            "New Feature",
-            "Proposal to introduce new features",
-            currentDay + 14 days
-        );
+        // address user = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
+        //
+        // lcToken.transfer(user, 300 ether);
+        //
+        // uint256 currentDay = block.timestamp / 1 days;
+        // governance.createProposal(
+        //     "Increase Rewards",
+        //     "Proposal to increase reward distribution by 10%",
+        //     currentDay + 7 days
+        // );
+        // governance.createProposal(
+        //     "Reduce Fees",
+        //     "Proposal to reduce transaction fees",
+        //     currentDay + 10 days
+        // );
+        // governance.createProposal(
+        //     "New Feature",
+        //     "Proposal to introduce new features",
+        //     currentDay + 14 days
+        // );
 
         vm.stopBroadcast();
 
